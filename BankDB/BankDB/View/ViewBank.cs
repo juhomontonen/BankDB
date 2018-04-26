e5bedc2885ef32ab5d6d6b273be34cf2308665ee
juhomontonen <juhop.montonen@gmail.com>
2018-04-26 12:58:47 +0300

    Added ViewBank.cs

---
+++ BankDB/BankDB/View/ViewBank.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BankDB.Models;
+using System.Data.SqlTypes;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using static BankDB.Repositories.CustomerRepository;
+using BankDB.Repositories;
+
+namespace BankDB.View
+{
+    class ViewBank
+    {
+        private readonly CustomerRepository customerRepository = new CustomerRepository();
+
+        public void PrintCustomer()
+        {
+            var customers = customerRepository.GetBankCustomers();
+            foreach (var c in customers)
+            {
+                Console.WriteLine(c.ToString());
+            }
+        }
+    }
+}
